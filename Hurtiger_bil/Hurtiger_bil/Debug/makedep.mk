b8f9102b0b856424e7abc766f939aa1ef5306b9e
AnonymousNyanCat4Leif <58462733+AnonymousNyanCat4Leif@users.noreply.github.com>
2026-04-27 13:54:09 +0200

    jio

---
+++ Hurtiger_bil/Hurtiger_bil/Debug/makedep.mk
@@ -0,0 +1,18 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+adc.c
+
+maalstreg.c
+
+main.c
+
+pwm.c
+
+Speedometer.c
+
+timer.c
+
+usart.c
+
